3b4e831fa17223e81b4bddc779dd0a6c651f0a5b
Steve Kim <ski@fb.com>
2016-10-12 21:31:33 -0700

    first commit

--- flicks/ViewController.swift
+++ flicks/ViewController.swift
@@ -8,18 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    @IBOutlet weak var moviesTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        moviesTableView.rowHeight = 100.0
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-
-
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView .dequeueReusableCellWithIdentifier("com.steve.protocell") as! MyCell
+        cell.myCustomLabel.text = "Row \(indexPath.row)"
+        return cell
+    }
+    
 }
 
+class MyCell: UITableViewCell {
+    @IBOutlet weak var myCustomLabel: UILabel!
+    
+}
